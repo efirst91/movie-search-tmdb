2376848f6ebdeb2b98a11b595564297e520675d7
efirst91 <eduardo.ha1991@gmail.com>
2023-10-28 07:16:07 -0500

    feature: creating folder structure

--- src/app/modules/movie/movie.module.ts
+++ src/app/modules/movie/movie.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {MovieDetailComponent} from "./components/movie-detail/movie-detail.component";
+import {MovieComponent} from "./movie.component";
 
 
 @NgModule({
   declarations: [
+    MovieComponent,
     MovieDetailComponent
   ],
   exports: [
