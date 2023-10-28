7620392a6e7637e070de9a40f98cdaaddf2f2474
efirst91 <eduardo.ha1991@gmail.com>
2023-10-28 09:43:11 -0500

    feat: adding custom token

--- src/app/modules/main/app.module.ts
+++ src/app/modules/main/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from '@modules/main/app.component';
 import {BaseLangComponent} from "@modules//base-lang/base-lang.component";
 import { HttpClientModule } from '@angular/common/http';
 import { TranslocoRootModule } from '../../config/i18n/transloco-root.module';
+import {API_URL} from "@shared/custom-tokens/custom-app-tokens";
+import {environment} from "@env/environment";
 
 @NgModule({
   declarations: [
@@ -18,7 +20,12 @@ import { TranslocoRootModule } from '../../config/i18n/transloco-root.module';
     HttpClientModule,
     TranslocoRootModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: API_URL,
+      useValue: environment.apiUrl
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
