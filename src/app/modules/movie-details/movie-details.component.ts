e56c634ab52c36137467aa139ff21bd6406d585a
efirst91 <eduardo.ha1991@gmail.com>
2023-10-29 11:07:39 -0500

    refactor: updating movie structure

---
+++ src/app/modules/movie-details/movie-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-movie-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.scss']
+})
+export class MovieDetailsComponent {
+
+}
