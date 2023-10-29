d83b70a7a91722a4f6a5be60380d3e25119ddeb3
efirst91 <eduardo.ha1991@gmail.com>
2023-10-29 02:09:01 -0500

    feat: updating imports and providers

--- src/app/modules/main/app.module.ts
+++ src/app/modules/main/app.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {APP_BASE_HREF} from "@angular/common";
 
-import { AppRoutingModule } from '@modules/main/app-routing.module';
-import { AppComponent } from '@modules/main/app.component';
-import {BaseLangComponent} from "@modules//base-lang/base-lang.component";
-import { HttpClientModule } from '@angular/common/http';
-import { TranslocoRootModule } from '../../config/i18n/transloco-root.module';
+import {TranslocoService} from "@ngneat/transloco";
+
 import {API_URL} from "@shared/custom-tokens/custom-app-tokens";
 import {environment} from "@env/environment";
+import {AppRoutingModule} from '@modules/main/app-routing.module';
+import {AppComponent} from '@modules/main/app.component';
+import {TranslocoRootModule} from '@config/i18n/transloco-root.module';
+import {NotFoundComponent} from "@shared/components/not-found/not-found.component";
+import {SearchComponent} from "@modules/search/search.component";
+import {TokenAdminComponent} from "@modules/token-admin/token-admin.component";
+import {LocalStorageService} from "@shared/services/local-storage/local-storage.service";
+
+
+export const getBaseHref = (tLocoService: TranslocoService, lStorageService:LocalStorageService) => {
+  return lStorageService.getActiveLang() || tLocoService.getActiveLang();
+};
+
 
 @NgModule({
   declarations: [
@@ -16,16 +28,26 @@ import {environment} from "@env/environment";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BaseLangComponent,
     HttpClientModule,
-    TranslocoRootModule
+    NotFoundComponent,
+    SearchComponent,
+
+    TranslocoRootModule,
+    TokenAdminComponent
   ],
   providers: [
     {
       provide: API_URL,
       useValue: environment.apiUrl
+    },
+    {
+      provide: APP_BASE_HREF,
+      useFactory: getBaseHref,
+      deps: [TranslocoService, LocalStorageService],
     }
+
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
