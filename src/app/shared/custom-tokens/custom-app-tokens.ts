01a93c625ac2816901995a1318f84b2b0003fc88
efirst91 <eduardo.ha1991@gmail.com>
2023-10-29 02:10:21 -0500

    feat: adding global custom token

--- src/app/shared/custom-tokens/custom-app-tokens.ts
+++ src/app/shared/custom-tokens/custom-app-tokens.ts
@@ -1,3 +1,6 @@
-import {InjectionToken} from "@angular/core";
+import {ErrorHandler, InjectionToken} from "@angular/core";
 
 export const API_URL = new InjectionToken<string>('API_URL');
+export const ERROR_HANDLERS = new InjectionToken<ErrorHandler[]>(
+  'ERROR_HANDLERS'
+);
