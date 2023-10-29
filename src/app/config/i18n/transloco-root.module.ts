880d6b37e65dc4879ccbbdc74a64294acd04d5d4
efirst91 <eduardo.ha1991@gmail.com>
2023-10-29 02:14:05 -0500

    refactor: formatting some files

--- src/app/config/i18n/transloco-root.module.ts
+++ src/app/config/i18n/transloco-root.module.ts
@@ -2,26 +2,27 @@ import {
   provideTransloco,
   TranslocoModule
 } from '@ngneat/transloco';
-import { isDevMode, NgModule } from '@angular/core';
-import { TranslocoHttpLoader } from './transloco-loader';
+import {isDevMode, NgModule} from '@angular/core';
+import {TranslocoHttpLoader} from './transloco-loader';
 
 
 @NgModule({
-  exports: [ TranslocoModule ],
+  exports: [TranslocoModule],
   providers: [
-      provideTransloco({
-        config: {
-          availableLangs: ['en', 'es'],
-          defaultLang: 'en',
-          // Remove this option if your application doesn't support changing language in runtime.
-          reRenderOnLangChange: true,
-          prodMode: !isDevMode(),
-          flatten: {
-            aot: !isDevMode()
-          }
-        },
-        loader: TranslocoHttpLoader
-      }),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'es'],
+        defaultLang: 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+        flatten: {
+          aot: !isDevMode()
+        }
+      },
+      loader: TranslocoHttpLoader
+    })
   ],
 })
-export class TranslocoRootModule {}
+export class TranslocoRootModule {
+}
