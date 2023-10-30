414ea450c6ad6f291dc4006173eb76a3deaebec3
efirst91 <eduardo.ha1991@gmail.com>
2023-10-30 08:59:39 -0500

    refactor: adding notification service

--- src/app/shared/services/snack-bar-notifications/snack-bar-notifications.service.ts
+++ src/app/shared/services/snack-bar-notifications/snack-bar-notifications.service.ts
@@ -1,8 +1,23 @@
 import {inject, Injectable} from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackBarNotificationsService {
+  readonly snackBar = inject(MatSnackBar);
 
+  /**
+   * Message notification
+   * @param message message text
+   * @param action action
+   * @example close
+   */
+  openSnackBar(message: string ='', action: string = 'close') {
+    this.snackBar.open(message, action, {
+      duration: 5000,
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+    });
+  }
 }
