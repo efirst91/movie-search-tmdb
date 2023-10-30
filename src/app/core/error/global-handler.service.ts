2ceec31d9b6213dcc1199890662cb6471bfd8f12
efirst91 <eduardo.ha1991@gmail.com>
2023-10-30 08:49:21 -0500

    feat: update error handler

--- src/app/core/error/global-handler.service.ts
+++ src/app/core/error/global-handler.service.ts
@@ -1,15 +1,22 @@
-import {ErrorHandler, inject, Injectable} from '@angular/core';
+import {ErrorHandler, inject, Injectable, NgZone} from '@angular/core';
 
-import {ERROR_HANDLERS} from "@shared/custom-tokens/custom-app-tokens";
+import {TranslocoService} from "@ngneat/transloco";
+import {SnackBarNotificationsService} from "@shared/services/snack-bar-notifications/snack-bar-notifications.service";
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class GlobalHandlerService implements ErrorHandler{
-  readonly #handlers = inject(ERROR_HANDLERS) ?? [];
+export class GlobalHandlerService implements ErrorHandler {
+  readonly zone = inject(NgZone);
+  snackNotification = inject(SnackBarNotificationsService);
+  tlService = inject(TranslocoService);
 
   handleError(error: unknown): void {
-    this.#handlers.forEach(handle => handle.handleError(error));
+    this.zone.run(() => {
+      this.snackNotification.openSnackBar(this.tlService.translate('An error has occurred we are working on it'),
+        this.tlService.translate('Close'));
+    });
+    console.warn('Error detail ->', error)
   }
 }
