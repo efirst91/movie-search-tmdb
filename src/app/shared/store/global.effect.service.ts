74ee153ee381752202a6bd6f63461453f36b94d3
efirst91 <eduardo.ha1991@gmail.com>
2023-11-06 18:20:26 -0500

    fix: localize the local error

--- src/app/shared/store/global.effect.service.ts
+++ src/app/shared/store/global.effect.service.ts
@@ -4,6 +4,9 @@ import {MoviesService} from "@data-access/movies.service";
 import {map, mergeMap} from "rxjs/operators";
 
 import * as actions from "./global.actions"
+import {catchError, EMPTY} from "rxjs";
+import {SnackBarNotificationsService} from "@shared/services/snack-bar-notifications/snack-bar-notifications.service";
+import {TranslocoService} from "@ngneat/transloco";
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +18,22 @@ export class GlobalEffects {
     mergeMap((action) => this.moviesService.getData(action.search)
       .pipe(
         map(response => actions.searchSuccess({criteria: action.search, data: response}),
-        )
+        ),
+        catchError(err => {
+          const errorMsg = this.tlService.translate("Can´t load the movies withe the specific search, " +
+            "please review your search query");
+          const actionMsg = this.tlService.translate('Cancel');
+          this.notificationS.openSnackBar(errorMsg, actionMsg, 6000);
+          return EMPTY;
+        })
       )
     )))
 
   constructor(
     private actions$: Actions,
     private moviesService: MoviesService,
+    private tlService: TranslocoService,
+    private notificationS: SnackBarNotificationsService
   ) {
   }
 }
