4a8de14bc5c5de19afe46ff729f4ea17c56cb87f
efirst91 <eduardo.ha1991@gmail.com>
2023-11-06 18:26:05 -0500

    fix: adding token admin test

--- src/app/modules/token-admin/token-admin.component.spec.ts
+++ src/app/modules/token-admin/token-admin.component.spec.ts
@@ -1,21 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgZone} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
 
-import { TokenAdminComponent } from './token-admin.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
 
-xdescribe('TokenAdminComponent', () => {
+import { TranslocoService} from "@ngneat/transloco";
+
+import {TokenAdminComponent} from './token-admin.component';
+import {TranslocoRootModule} from "@config/i18n/transloco-root.module";
+import {LocalStorageService} from "@shared/services/local-storage/local-storage.service";
+import {SnackBarNotificationsService} from "@shared/services/snack-bar-notifications/snack-bar-notifications.service";
+
+describe('TokenAdminComponent', () => {
   let component: TokenAdminComponent;
   let fixture: ComponentFixture<TokenAdminComponent>;
+  let localStorageService: LocalStorageService;
+  let snackBarService: SnackBarNotificationsService;
+  let router: Router;
+  let ngZone: NgZone;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [TokenAdminComponent]
+      declarations: [],
+      imports: [
+        CommonModule,BrowserAnimationsModule, MatInputModule,
+        MatButtonModule, ReactiveFormsModule,
+        FormsModule, TranslocoRootModule,
+        MatSnackBarModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        TokenAdminComponent,
+        TranslocoService,
+        LocalStorageService,
+        SnackBarNotificationsService,
+        MatSnackBar,
+        Router,
+      ]
     });
+
     fixture = TestBed.createComponent(TokenAdminComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    localStorageService = TestBed.inject(LocalStorageService);
+    snackBarService = TestBed.inject(SnackBarNotificationsService);
+    router = TestBed.inject(Router);
+    ngZone = TestBed.inject(NgZone);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('onUpdateToken should update the token', () => {
+    const token = 'testToken';
+    component.tokenControl = new FormControl(token);
+
+    const setTokenSpy = jest.spyOn(localStorageService, 'setToken').mockReturnValue(undefined);
+    const openSnackBarSpy = jest.spyOn(snackBarService, 'openSnackBar').mockReturnValue(undefined);
+    const navigateSpy = jest.spyOn(router, 'navigate').mockReturnValue(Promise.resolve(true));
+
+    component.onUpdateToken();
+
+    expect(setTokenSpy).toHaveBeenCalledWith(token);
+    expect(openSnackBarSpy).toHaveBeenCalledWith('Token updated!!', 'Close');
+    expect(navigateSpy).toHaveBeenCalled();
+
+    setTokenSpy.mockRestore();
+    openSnackBarSpy.mockRestore();
+    navigateSpy.mockRestore();
   });
 });
--- src/app/modules/token-admin/token-admin.component.ts
+++ src/app/modules/token-admin/token-admin.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
@@ -26,17 +26,19 @@ export class TokenAdminComponent {
     private lStorageService: LocalStorageService,
     private notification: SnackBarNotificationsService,
     private tlService: TranslocoService,
-    private router: Router
+    private router: Router,
+    private ngZone: NgZone
   ) {
     this.tokenControl = new FormControl<string | null>(null, [Validators.required]);
   }
 
   onUpdateToken(): void {
     console.log('value form -> ', this.tokenControl.value)
-    debugger
     const token = this.tokenControl.value.trim()
     this.lStorageService.setToken(token);
-    this.notification.openSnackBar(this.tlService.translate('Token updated!!', this.tlService.translate('Close')))
-    this.router.navigate(['/search']).then();
+    this.notification.openSnackBar(this.tlService.translate('Token updated!!'), this.tlService.translate('Close'));
+    this.ngZone.run(() => {
+      this.router.navigate(['/search']).then();
+    })
   }
 }
