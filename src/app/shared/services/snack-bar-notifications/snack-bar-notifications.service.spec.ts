ebd0bdc8c952864818cc8fbe105b2ce387d10d98
efirst91 <eduardo.ha1991@gmail.com>
2023-11-06 18:24:57 -0500

    fix: fix snack bar notification

--- src/app/shared/services/snack-bar-notifications/snack-bar-notifications.service.spec.ts
+++ src/app/shared/services/snack-bar-notifications/snack-bar-notifications.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SnackBarNotificationsService } from './snack-bar-notifications.service';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 xdescribe('SnackBarNotificationsService', () => {
   let service: SnackBarNotificationsService;
+  let snackBar: MatSnackBar;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SnackBarNotificationsService],
+    });
     service = TestBed.inject(SnackBarNotificationsService);
+    snackBar = TestBed.inject(MatSnackBar);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should open a snack bar with default values', () => {
+    const openSpy = spyOn(snackBar, 'open');
+
+    service.openSnackBar();
+
+    expect(openSpy).toHaveBeenCalledWith('', 'close', 5000, 'top', 'right');
+  });
+
 });
--- src/app/shared/services/snack-bar-notifications/snack-bar-notifications.service.ts
+++ src/app/shared/services/snack-bar-notifications/snack-bar-notifications.service.ts
@@ -1,5 +1,10 @@
 import {inject, Injectable} from '@angular/core';
-import {MatSnackBar} from "@angular/material/snack-bar";
+import {
+  MatSnackBar,
+  MatSnackBarConfig,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition
+} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +16,18 @@ export class SnackBarNotificationsService {
    * Message notification
    * @param message message text
    * @param action action
+   * @param duration message duration
+   * @param vP Vertical position can be top or bottom
+   * @param hP Horizontal position can be 'start' | 'center' | 'end' | 'left' | 'right'
    * @example close
    */
-  openSnackBar(message: string ='', action: string = 'close') {
-    this.snackBar.open(message, action, {
-      duration: 5000,
-      verticalPosition: 'top',
-      horizontalPosition: 'right',
-    });
+  openSnackBar(message: string = '', action: string = 'close',
+               duration = 5000,vP:MatSnackBarVerticalPosition = 'top',
+               hP:MatSnackBarHorizontalPosition=  'right') {
+    const newConfig = new MatSnackBarConfig()
+    newConfig.duration = duration;
+    newConfig.verticalPosition = vP ;
+    newConfig.horizontalPosition = hP;
+    this.snackBar.open(message, action, newConfig);
   }
 }
